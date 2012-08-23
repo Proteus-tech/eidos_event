55444c0d9a10303cf9389c6e93f0a150a6edc2b1
euam <euam@proteus-tech.com>
2012-08-24 01:40:51 +0700

    remove piston from install requires increase version number

--- setup.py
+++ setup.py
@@ -7,14 +7,14 @@ from distutils.core import setup
 
 setup(
     name='eidos_event',
-    version='0.0.1',
+    version='0.0.2',
     description='Event library for Eidos',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
     url='http://proteus-tech.com',
     long_description = read('README.md'),
     # django is Eidos current version of django
-    install_requires=['django==1.3.1', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5', 'django-piston==0.2.3', 'django-softdelete==0.3.4'],
+    install_requires=['django==1.3.1', 'djangorestframework>=0.3.3', 'django-serene>=0.0.5'],
 #    package_data={'restful': ['templates/restful/*.html']},
     packages=['event',],
 )
