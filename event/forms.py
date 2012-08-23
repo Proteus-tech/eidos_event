fc98cb52650e82a4cbf39639a6057f5eaa229fbd
euam <euam@proteus-tech.com>
2012-08-24 00:48:47 +0700

    remove event types from form

--- event/forms.py
+++ event/forms.py
@@ -2,10 +2,6 @@
 from django import forms
 
 from event.models import Event
-from sample_app import event_types
-
-def get_event_types():
-    return [(event_type, event_type) for event_type in event_types.__all__]
 
 class EventForm(forms.ModelForm):
     class Meta:
