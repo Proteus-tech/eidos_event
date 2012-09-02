9bf9f014e0e790f9f26994feaafe2cbf9749888d
Pam Suebvisai <sinapam@proteus-tech.com>
2012-09-02 23:45:57 +0700

    change version number to 0.0.14

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='eidos_event',
-    version='0.0.13',
+    version='0.0.14',
     description='Event library for Eidos',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
