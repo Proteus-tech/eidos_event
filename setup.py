c64c02e065033802b6c4aeab1e0bdbc89b7f72f7
Pam Suebvisai <sinapam@proteus-tech.com>
2012-09-06 19:49:49 +0700

    change version to 0.0.20

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='eidos_event',
-    version='0.0.19',
+    version='0.0.20',
     description='Event library for Eidos',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
