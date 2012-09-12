5c80a4cf008119402a226a17cf27b9910a0d65dd
Pam Suebvisai <sinapam@proteus-tech.com>
2012-09-12 14:38:40 +0700

    use pypi for gevent-socketio kor dai neeeeee

--- setup.py
+++ setup.py
@@ -7,15 +7,15 @@ from distutils.core import setup
 
 setup(
     name='eidos_event',
-    version='0.0.30',
+    version='0.0.31',
     description='Event library for Eidos',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
     url='http://proteus-tech.com',
     long_description = read('README.md'),
     # django is Eidos current version of django
-    install_requires=['django>=1.3.1', 'django-serene>=0.0.5'],
-    dependency_links=['http://github.com/proteus-tech/django-logger/tarball/master#egg=django-logger-0.1', 'http://github.com/abourget/gevent-socketio/tarball/master#egg=gevent-socketio-0.3.5-rc2'],
+    install_requires=['django>=1.3.1', 'django-serene>=0.0.5', 'gevent-socketio==0.3.5-rc2'],
+    dependency_links=['http://github.com/proteus-tech/django-logger/tarball/master#egg=django-logger-0.1'],
     package_data={'event': ['templates/*.html']},
     packages=['event', 'event.migrations'],
 )
