69df4c81c1c531daedf9b15e560d2b3ced84f5b4
Pam Suebvisai <sinapam@proteus-tech.com>
2012-09-12 14:32:18 +0700

    try fixing dependency_links

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='eidos_event',
-    version='0.0.29',
+    version='0.0.30',
     description='Event library for Eidos',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
@@ -15,7 +15,7 @@ setup(
     long_description = read('README.md'),
     # django is Eidos current version of django
     install_requires=['django>=1.3.1', 'django-serene>=0.0.5'],
-    dependency_links=['http://github.com/proteus-tech/django-logger/tarball/master', 'http://github.com/abourget/gevent-socketio/tarball/master'],
+    dependency_links=['http://github.com/proteus-tech/django-logger/tarball/master#egg=django-logger-0.1', 'http://github.com/abourget/gevent-socketio/tarball/master#egg=gevent-socketio-0.3.5-rc2'],
     package_data={'event': ['templates/*.html']},
     packages=['event', 'event.migrations'],
 )
