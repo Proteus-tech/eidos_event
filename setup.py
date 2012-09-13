460aa3f6957d620beb1ba621ed2892685cc18591
Pam Suebvisai <sinapam@proteus-tech.com>
2012-09-13 22:41:10 +0700

    changed version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='eidos_event',
-    version='0.0.33',
+    version='0.0.34',
     description='Event library for Eidos',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
