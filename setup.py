15d15b64f5c9aef6dfbd47f39d4b8ce033c610e7
champ <kriangsak@proteus-agility.com>
2013-01-22 20:28:57 +0700

    update version to 0.0.51

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='eidos_event',
-    version='0.0.50',
+    version='0.0.51',
     description='Event library for Eidos',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
