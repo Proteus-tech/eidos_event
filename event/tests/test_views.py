7e996403363654e58db1d1d6b9156e9e80b26467
Pam Suebvisai <sinapam@proteus-tech.com>
2013-01-24 21:20:11 +0700

    no need to send user and cookies now

--- event/tests/test_views.py
+++ event/tests/test_views.py
@@ -218,7 +218,7 @@ class TestAfterEventSave(EventTestBase):
         event = Event.objects.create(event_type='MyEvent11', resource='http://storyhost/PAM-1',
             project='http://projecthost/project/PAM', data='{}')
         self.assertEqual(self.mock_send_task.call_args[0][0], 'tasks.tasks.process_event')
-        self.assertEqual(self.mock_send_task.call_args[1]['args'], [event, self.mock_request.user, self.mock_request.COOKIES])
+        self.assertEqual(self.mock_send_task.call_args[1]['args'], [event])
 
 
 
@@ -274,4 +274,4 @@ class TestAddEventTask(EventTestBase):
             project='http://projecthost/project/PAM', data='{}')
         add_event_task(event)
         self.assertEqual(self.mock_send_task.call_args[0][0], 'tasks.tasks.process_event')
-        self.assertEqual(self.mock_send_task.call_args[1]['args'], [event, self.mock_request.user, self.mock_request.COOKIES])
+        self.assertEqual(self.mock_send_task.call_args[1]['args'], [event])
