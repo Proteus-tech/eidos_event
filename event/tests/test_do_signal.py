5b9533f8ecc320246ebd6a0a1ac6e03cc997f1b8
Pam Suebvisai <sinapam@proteus-tech.com>
2013-01-31 15:31:47 +0700

    added TestEmitToChannel

--- event/tests/test_do_signal.py
+++ event/tests/test_do_signal.py
@@ -2,7 +2,7 @@
 from datetime import datetime, timedelta
 from mock import patch
 from redis.exceptions import ConnectionError
-from event.do_signal import add_event_task
+from event.do_signal import add_event_task, emit_to_channel
 from event.models import Event
 from event.tests.base import EventTestBase
 
@@ -41,3 +41,23 @@ class TestAddEventTask(EventTestBase):
             })
         self.assertEqual(mock_logger_error.call_args[0][0], 'Redis does not seem to be running')
 
+class TestEmitToChannel(EventTestBase):
+
+    def test_emit_to_channel_event_is_publised_correctly(self):
+        self.assertFalse(self.mock_redis_publish.called)
+        event = Event(event_type='MyEvent', resource='http://storyhost/PAM-1',
+            project='http://projecthost/project/PAM', data='{}',
+            created_on=datetime(2013, 1, 31), created_by='http://authhost/user/testuser')
+        emit_to_channel('http://projecthost/project/PAM', 'new_event', event.created_by, event.serialize())
+        self.assertEqual(self.mock_redis.call_args[1]['host'], 'localhost')
+        self.assertEqual(self.mock_redis.call_args[1]['port'], 6379)
+        self.assertEqual(self.mock_redis.call_args[1]['db'], 0)
+        self.assertEqual(self.mock_redis_publish.call_args[0][0], 'socketio_http://projecthost/project/PAM')
+
+        publish_msg = self.mock_redis_publish.call_args[0][1]
+        self.assertIn('{"args": ["http://projecthost/project/PAM", "http://authhost/user/testuser", ', publish_msg)
+        self.assertIn('"project": "http://projecthost/project/PAM"', publish_msg)
+        self.assertIn('"resource": "http://storyhost/PAM-1"', publish_msg)
+        self.assertIn('"event_type": "MyEvent"', publish_msg)
+        self.assertIn('"data": "{}"', publish_msg)
+        self.assertIn('"name": "new_event"', publish_msg)
