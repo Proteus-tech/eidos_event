34ed4772c49b70c8f7d4924778d7b58733977af4
siraset jirapatchandej <vernomcrp@gmail.com>
2013-08-01 14:54:19 +0700

    Update setup.py
    Downgrade django-celery to 3.0.11 (original v.), but specified pytz to 2013b

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from distutils.core import setup
 
 setup(
     name='eidos_event',
-    version='0.0.58',
+    version='0.0.59',
     description='Event library for Eidos',
     author='Proteus Technologies',
     author_email='team@proteus-tech.com',
