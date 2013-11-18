3859f1ab140c37a8838d9f018beffda11496b6f9
Pam <sinapam@proteus-agility.com>
2013-11-18 22:00:36 +0700

    fixed the way to import urls module for django 1.3.1

--- event/urls.py
+++ event/urls.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.conf.urls import patterns, url
+from django.conf.urls.defaults import patterns, url  # we are still using defaults for django 1.3.1
 from serene.views import InstanceModelView
 from .resources import EventResource
 from .views import EventListView
